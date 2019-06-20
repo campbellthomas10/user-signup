6680ce778c1415980a2bf8c5df253484a0d36e98
Tommy Campbell <tjc2187@gmail.com>
2019-06-19 20:53:09 -0500

    made it look cleaner

--- main.py
+++ main.py
@@ -32,19 +32,28 @@ def checks():
     verification_error = ''
     email_error = ''
 
+
+
+    #Checks if username is valid
     if not is_valid(username):
         username_error = "Username is invalid!"
         username = ''
 
 
+
+    #Checks if password is valid
     if not is_valid(password):
         password_error = "Password is invalid!"
 
-    
+
+
+    #Checks if the password and its verification are the same
     if password != verified:
         verification_error = "Passwords must match!"
 
 
+
+    #Checks if the email is valid (if there is one)
     if len(email) != 0:
         if email.find('@') != -1:
             if email.find('.') == -1:
@@ -53,7 +62,10 @@ def checks():
         else:
             email_error = "Email is invalid!"
             email = ''
-    
+
+
+
+    #Checks if all fields are valid, resets invalid fields
     if not username_error and not password_error and not verification_error and not email_error:
         return redirect('/signed-up?username={0}'.format(username))
     else:
@@ -65,6 +77,9 @@ def checks():
         username=username,
         email=email)
 
+
+
+#What shows up when all is valid
 @app.route('/signed-up')
 def signed_up():
     username = request.args.get('username')
